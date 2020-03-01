7a764bb68b7830c2dc177e1a7bcf19104eb0cf4b
“karimshakir” <karimshakir99@gmail.com>
2020-03-01 15:33:58 -0600

    connect jpa to mysql

---
+++ src/main/java/com/krew/jpamysql/UserRepository.java
@@ -0,0 +1,7 @@
+package com.krew.jpamysql;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+}
---
+++ src/test/java/com/krew/jpamysql/JpaMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krew.jpamysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
